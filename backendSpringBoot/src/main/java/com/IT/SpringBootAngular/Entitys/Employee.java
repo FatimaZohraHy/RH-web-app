6a53c2f0b5f99b4a7c408d97cc3519e790a36e5a
Ouaaqa Youssef <you@example.com>
2023-12-05 18:11:25 +0100

    Initial commit

---
+++ backendSpringBoot/src/main/java/com/IT/SpringBootAngular/Entitys/Employee.java
@@ -0,0 +1,154 @@
+package com.IT.SpringBootAngular.Entitys;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+
+@Document(collection = "employees")
+public class Employee {
+    @Id
+    private String _id;
+    private String firstName;
+    private String lastName;
+    private String position;
+    private String email;
+    private String phoneNumber;
+    private Date birthDate;
+    private String address;
+    private String city;
+    private String state;
+    private String department;
+    private Date hireDate;
+    private double salary;
+    private boolean isActive;
+
+    public Employee(String _id, String firstName, String lastName, String position, String email, String phoneNumber, Date birthDate, String address, String city, String state, String department, Date hireDate, double salary, boolean isActive) {
+        this._id = _id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.position = position;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.birthDate = birthDate;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.department = department;
+        this.hireDate = hireDate;
+        this.salary = salary;
+        this.isActive = isActive;
+    }
+
+    public String get_id() {
+        return _id;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public Date getHireDate() {
+        return hireDate;
+    }
+
+    public void setHireDate(Date hireDate) {
+        this.hireDate = hireDate;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+}
---
+++ backendSpringBoot/src/main/java/com/IT/SpringBootAngular/Entitys/User.java
@@ -0,0 +1,38 @@
+package com.IT.SpringBootAngular.Entitys;
+
+import org.springframework.data.annotation.Id;
+
+public class User {
+
+    @Id
+    private int id;
+    private String email;
+    private String password;
+
+    public User(int id, String email, String password) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    private String getPassword() {
+        return password;
+    }
+
+}
